931dd2dd66f879acef1f6bcf00542394379a3cef
kaspvik <kasper.vikstrom@medieinstituet.se>
2025-02-19 15:35:20 +0100

    Add SiteLayout component and set up routing for HomePage and PosterPage

---
+++ src/SiteLayout.tsx
@@ -0,0 +1,14 @@
+import { Outlet } from "react-router";
+
+export default function SiteLayout() {
+  return (
+    <div>
+      <header>
+        <h1></h1>
+      </header>
+      <div>
+        <main></main>
+      </div>
+    </div>
+  );
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,19 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router";
+import { HomePage } from "./pages/HomePage";
+import { SiteLayout } from "./SiteLayout";
+import { PosterPage } from "./pages/PosterPage";
 
-createRoot(document.getElementById("root")!).render(<StrictMode></StrictMode>);
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={SiteLayout />}>
+          <Route index element={HomePage}/>
+          <Route path="poster/:slug" element={PosterPage}/>
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
+);
