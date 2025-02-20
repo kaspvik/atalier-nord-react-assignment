eda5d69e01963ce5d9969f62f889cbbd9b46f9e9
kaspvik <kasper.vikstrom@medieinstituet.se>
2025-02-20 15:19:04 +0100

    Include a dropdown menu and update routing for PosterPage

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+import { Link } from "react-router";
 import styled from "styled-components";
 
 const HeaderContainer = styled.header`
@@ -9,6 +11,7 @@ const HeaderContainer = styled.header`
   padding: 1rem;
   font-family: "Aileron Black", sans-serif;
   font-size: 1.5rem;
+  position: relative;
 `;
 
 const Logo = styled.h1`
@@ -18,18 +21,61 @@ const Logo = styled.h1`
 const MenuIcon = styled.span`
   font-size: 4rem;
   cursor: pointer;
-  color: electricblue;
 
   &:hover {
-    color: rgb(125, 129, 255);
+    color: rgb(123, 156, 255);
+  }
+`;
+
+const Menu = styled.nav<{ open: boolean }>`
+  position: absolute;
+  top: 100%;
+  left: 0;
+  background-color: white;
+  width: 200px;
+  display: ${({ open }) => (open ? "block" : "none")};
+  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+  padding: 1rem;
+  border-radius: 8px;
+`;
+
+const MenuList = styled.ul`
+  list-style: none;
+  padding: 0;
+  margin: 0;
+`;
+
+const MenuItem = styled.li`
+  padding: 0.5rem 0;
+
+  a {
+    text-decoration: none;
+    color: #0746fd;
+    font-size: 1.2rem;
+
+    &:hover {
+      color: rgb(123, 156, 255);
+    }
   }
 `;
 
 export default function Header() {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <HeaderContainer>
-      <MenuIcon>=</MenuIcon>
+      <MenuIcon onClick={() => setIsOpen(!isOpen)}>=</MenuIcon>
       <Logo>ATELIER NORD</Logo>
+      <Menu open={isOpen}>
+        <MenuList>
+          <MenuItem>
+            <Link to="/">Home</Link>
+          </MenuItem>
+          <MenuItem>
+            <Link to="/posters">Posters</Link>
+          </MenuItem>
+        </MenuList>
+      </Menu>
     </HeaderContainer>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -12,7 +12,7 @@ createRoot(document.getElementById("root")!).render(
       <Routes>
         <Route path="/" element={<App />}>
           <Route index element={<HomePage />} />
-          <Route path="/poster" element={<PosterPage />} />
+          <Route path="/posters" element={<PosterPage />} />
         </Route>
       </Routes>
     </BrowserRouter>
