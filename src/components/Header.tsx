30a87ba31d91c4d5f297f34e28e9d58260aba59c
kaspvik <kasper.vikstrom@medieinstituet.se>
2025-02-20 14:07:47 +0100

    Testing the styled-components for styling and layout on the Header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,35 @@
+import styled from "styled-components";
+
+const HeaderContainer = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: rgb(237, 237, 237);
+  color: #0746fd;
+  padding: 1rem;
+  font-family: "Aileron Black", sans-serif;
+  font-size: 1.5rem;
+`;
+
+const Logo = styled.h1`
+  margin: 0;
+`;
+
+const MenuIcon = styled.span`
+  font-size: 4rem;
+  cursor: pointer;
+  color: electricblue;
+
+  &:hover {
+    color: rgb(125, 129, 255);
+  }
+`;
+
 export default function Header() {
   return (
-    <header className="flex p-4 bg-slate-200 justify-between">
-      <h1>Header</h1>
-      <span>🍔</span>
-    </header>
+    <HeaderContainer>
+      <MenuIcon>=</MenuIcon>
+      <Logo>ATELIER NORD</Logo>
+    </HeaderContainer>
   );
 }
