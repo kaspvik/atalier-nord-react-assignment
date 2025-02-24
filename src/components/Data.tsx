b7d2eb27858f04a8a4c8414230237d0c1d7d26a4
kaspvik <kasper.vikstrom@medieinstituet.se>
2025-02-24 15:23:25 +0100

    Add posters data for display

---
+++ src/components/Data.tsx
@@ -0,0 +1,37 @@
+export const posters = [
+  {
+    id: 1,
+    title: "Primary",
+    image: "src/assets/DisplayPosters/PrimaryDisplay2.png",
+  },
+  {
+    id: 2,
+    title: "Candy",
+    image: "src/assets/DisplayPosters/CandyDisplay2.png",
+  },
+  {
+    id: 3,
+    title: "Glaucus",
+    image: "src/assets/DisplayPosters/GlaucusDisplay2.png",
+  },
+  {
+    id: 4,
+    title: "Glacies",
+    image: "src/assets/DisplayPosters/GlaciesDisplay2.png",
+  },
+  {
+    id: 5,
+    title: "Lilith",
+    image: "src/assets/DisplayPosters/LilithDisplay2.png",
+  },
+  {
+    id: 6,
+    title: "Prisma",
+    image: "src/assets/DisplayPosters/PrismaDisplay2.png",
+  },
+  {
+    id: 7,
+    title: "Violett",
+    image: "src/assets/DisplayPosters/ViolettDisplay2.png",
+  },
+];
