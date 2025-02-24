a6ea4cb3bbebb26c34ffc42c2ed13de639135c5d
kaspvik <kasper.vikstrom@medieinstituet.se>
2025-02-24 17:40:01 +0100

    Add PosterCard component and integrate it into HomePage with styled-components

---
+++ src/components/PosterCard.tsx
@@ -0,0 +1,43 @@
+import { Link } from "react-router";
+import styled from "styled-components";
+
+const Card = styled.div`
+  width: 300px;
+  border: 2px solid #000;
+  padding: 16px;
+  text-align: center;
+  cursor: pointer;
+  transition: 0.3s;
+
+  &:hover {
+    transform: scale(1.05);
+  }
+`;
+
+const PosterImage = styled.img`
+  width: 100%;
+  height: auto;
+  border-radius: 8px;
+`;
+
+const PosterTitle = styled.h3`
+  margin-top: 12px;
+  font-size: 18px;
+  font-family: "Aileron Black", sans-serif;
+`;
+
+interface PosterCardProps {
+  image: string;
+  title: string;
+}
+
+export default function PosterCard({ image, title }: PosterCardProps) {
+  return (
+    <Link to="/posters" style={{ textDecoration: "none", color: "inherit" }}>
+      <Card>
+        <PosterImage src={image} alt={title} />
+        <PosterTitle>{title}</PosterTitle>
+      </Card>
+    </Link>
+  );
+}
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,9 +1,28 @@
+import styled from "styled-components";
 import WeatherPoster from "../api/WeatherPoster";
+import PosterCard from "../components/PosterCard";
 
-export default function HomePage() {
+const StyledHeading = styled.h2`
+  font-size: 1.8rem;
+  font-weight: bold;
+  text-transform: uppercase;
+  color: #333;
+  margin: 20px 0;
+  text-align: center;
+  font-family: "Aileron Black", sans-serif;
+`;
+
+const YourComponent = () => {
   return (
     <div>
       <WeatherPoster />
+      <StyledHeading>THE COLLECTIONS:</StyledHeading>
+      <PosterCard
+        image="src/assets/DisplayPosters/PrimaryDisplay2.png"
+        title="THE PRISMA COLLECTION"
+      />
     </div>
   );
-}
+};
+
+export default YourComponent;
